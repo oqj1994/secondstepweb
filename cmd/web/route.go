696d338a8315f42bcbec892725294d655c93a314
jia <carryonb@163.com>
2023-02-03 10:34:10 +0800

    "add a http static file server"

--- cmd/web/route.go
+++ cmd/web/route.go
@@ -4,6 +4,7 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/vitaLemoTea/secondstepweb/pkg/handlers"
+	"net/http"
 )
 
 func NewRouter() chi.Router {
@@ -12,6 +13,8 @@ func NewRouter() chi.Router {
 	mux.Use(mymiddleware)
 	mux.Use(csrf)
 	mux.Use(sessionLoad)
+	fileServer := http.FileServer(http.Dir("./statics/"))
+	mux.Handle("/statics/*", http.StripPrefix("/statics/", fileServer))
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
 	return mux
